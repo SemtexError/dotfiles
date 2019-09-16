7b7a0941c3d1b7dcaa307ab668363173fb340372
Tempert <github@tempert.nu>
2019-09-16 22:58:57 +0200

    conditional polybar and i3

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,8 +12,12 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # polybar $polybarName &
 
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload $polybarName &
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        if [[ $(hostname -s) = "host" ]]; then
+            MONITOR=$m SHOWSCREEN=false polybar --reload $polybarName &
+        else
+            MONITOR=$m polybar --reload $polybarName &
+        fi
   done
 else
   polybar --reload $polybarName &
