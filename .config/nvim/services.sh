5330f0990b7ed59d3f5e439e84b5e98423b28ad7
Tempert <github@tempert.nu>
2019-11-13 21:55:57 +0100

    npm services

--- .config/nvim/services.sh
+++ .config/nvim/services.sh
@@ -13,10 +13,20 @@ install_software () {
     if [ -f "/usr/bin/apt" ]; then
        if [ -f "/usr/bin/dpkg" ]; then
            if ! dpkg -s $1 >/dev/null 2>&1; then
-               sudo apt install $1 -y
+               sudo /usr/bin/apt install $1 -y
            fi
        fi
     fi
 }
 
+install_npm () {
+    if [ -f "/usr/bin/npm" ]; then
+       if ! npm list --depth 1 --global $1 > /dev/null 2>&1; then
+           /usr/bin/npm i -g $1
+       fi
+    fi
+
+}
+
 install_software ripgrep
+install_npm vue-language-server
