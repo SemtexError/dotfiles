434f9c46e52ee42b8fc6121341b386fd8bb4f246
github@tempert.nu <github@tempert.nu>
2020-05-04 22:27:34 +0200

    feature()

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,24 +1,29 @@
-#!/usr/bin/env sh
+# !/bin/bash
 
-polybarName=main
+PRIMARY_BAR_NAME=main
+SECONDARY_BAR_NAME=secondary
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-# Launch polybar
-# polybar $polybarName &
+# Display main bar on primary monitor
+for m in $(polybar --list-monitors | grep primary | cut -d":" -f1); do
+    if [[ $(hostname -s) = "host" ]]; then
+        MONITOR=$m SHOWSCREEN=false polybar $PRIMARY_BAR_NAME &
+    else
+        MONITOR=$m polybar $PRIMARY_BAR_NAME &
+    fi
+done
 
-if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        if [[ $(hostname -s) = "host" ]]; then
-            MONITOR=$m SHOWSCREEN=false polybar --reload $polybarName &
-        else
-            MONITOR=$m polybar --reload $polybarName &
-        fi
-  done
-else
-  polybar --reload $polybarName &
-fi
+# Display secondary bar on secondary monitor(s)
+# This bar only contain the essentials
+for m in $(polybar --list-monitors | grep -v primary | cut -d":" -f1); do
+    if [[ $(hostname -s) = "host" ]]; then
+        MONITOR=$m SHOWSCREEN=false polybar $SECONDARY_BAR_NAME &
+    else
+        MONITOR=$m polybar $SECONDARY_BAR_NAME &
+    fi
+done
